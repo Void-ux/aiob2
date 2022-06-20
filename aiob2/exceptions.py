957237c36b44476ec3b9cd78ee92598fe30c1b69
Void-ux <daniel.grant2017@gmail.com>
2022-06-20 18:24:42 +0800

    More exceptions to support new methods.

--- aiob2/exceptions.py
+++ aiob2/exceptions.py
@@ -49,6 +49,22 @@ class B2RequestTimeout(B2Exception):
     """The service timed out reading the uploaded file."""
 
 
+class B2AccessDenied(B2Exception):
+    """The provided customer-managed encryption key is wrong."""
+
+
+class B2DownloadCapExceeded(B2Exception):
+    """Usage cap exceeded."""
+
+
+class B2NotFound(B2Exception):
+    """File is not in B2 Cloud Storage."""
+
+
+class B2RangeNotSatisfiable(B2Exception):
+    """The Range header in the request is valid but cannot be satisfied for the file."""
+
+
 class B2Error(NamedTuple):
     status: int
     code: str
@@ -64,10 +80,16 @@ codes = {
 
     B2Error(403, 'transaction_cap_exceeded'): B2TransactionCapExceeded,
     B2Error(403, 'storage_cap_exceeded'): B2StorageCapExceeded,
+    B2Error(403, 'access_denied'): B2AccessDenied,
+    B2Error(403, 'download_cap_exceeded'): B2DownloadCapExceeded,
+
+    B2Error(404, 'not_found'): B2NotFound,
 
     B2Error(405, 'method_not_allowed'): B2MethodNotAllowed,
 
     B2Error(408, 'request_timeout'): B2RequestTimeout,
+
+    B2Error(416, 'range_not_satisfiable'): B2RangeNotSatisfiable,
 
     B2Error(503, 'service_unavailable'): B2ServiceUnavailable
 
