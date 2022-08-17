6280e9c65d9563fc0288762cd1013bde8abf635d
Void-ux <daniel.grant2017@gmail.com>
2022-08-18 00:04:19 +0800

    Test Client ctx manager and session passing

---
+++ tests/test_client.py
@@ -0,0 +1,43 @@
+import asyncio
+import sys
+import os
+
+import aiohttp
+import pytest
+from pathlib import Path
+
+from aiob2 import Client, B2ConnectionInfo
+
+# For local tests
+if sys.platform == "win32":
+    with open('.env', 'r') as file:
+        print(file)
+        for row in file:
+            print(row)
+            row = row.split('=')
+            os.environ[row[0]] = row[1].replace('\n', '')
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+path = Path(__file__).resolve().parent / 'payloads/test_image.jpg'
+conn_info = B2ConnectionInfo(os.environ['KEY_ID'], os.environ['APP_ID'])
+bucket_id = os.environ['BUCKET_ID']
+
+
+class TestClient:
+    @pytest.mark.asyncio
+    async def test_actions(self):
+        # Testing that supplying an aiohttp session works, and the __aenter__ works
+        session = aiohttp.ClientSession()
+
+        async with Client(conn_info, session) as client:
+            file = await client.upload_file(
+                content_bytes=path.read_bytes(),
+                content_type='image/jpeg',
+                file_name='test.jpg',
+                bucket_id=bucket_id,
+            )
+
+            await client.delete_file(file_name=file.name, file_id=file.id)
+
+        assert client._http._session.closed
