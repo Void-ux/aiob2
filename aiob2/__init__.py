3f76453c75784009fad55e9cc7217bce1885168c
Void-ux <daniel.grant2017@gmail.com>
2022-08-22 18:28:19 +0800

    Version bump

--- aiob2/__init__.py
+++ aiob2/__init__.py
@@ -1,8 +1,7 @@
 from .exceptions import *
 from .models import *
-from .bucket import Client
 
 __title__ = 'aiob2'
 __author__ = 'Dan'
 __license__ = 'MIT'
-__version__ = '0.4.2'
+__version__ = '0.4.3'
