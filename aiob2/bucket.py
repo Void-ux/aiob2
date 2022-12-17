757994c81adacbe864551ba5b6aef63cfa799541
Void-ux <daniel.grant2017@gmail.com>
2022-12-17 23:39:13 +0800

    remove ConnectionInfo; refactor for new http.py

--- aiob2/bucket.py
+++ aiob2/bucket.py
@@ -1,20 +1,26 @@
-import aiohttp
-
 from typing import Optional
 
-from .models import *
+import aiohttp
+
+from .models import (
+    File,
+    DeletedFile,
+    DownloadedFile
+)
 from .http import HTTPClient
 
-__all__ = ['Client']
+__all__ = ('Client', )
 
 
 class Client:
-    def __init__(self, connection_info: B2ConnectionInfo, session: Optional[aiohttp.ClientSession] = None):
-        if not isinstance(connection_info, B2ConnectionInfo):
-            raise TypeError('aiob2.Client requires connection_info to be of type B2ConnectionInfo')
-            
-        self.connection_info = connection_info
-        self._http = HTTPClient(connection_info, session)
+    def __init__(
+        self,
+        application_key_id: str,
+        application_key: str,
+        *,
+        session: Optional[aiohttp.ClientSession] = None
+    ):
+        self._http = HTTPClient(application_key_id, application_key, session)
 
     async def __aenter__(self):
         return self
@@ -35,60 +41,55 @@ class Client:
         file_name: str,
         bucket_id: str
     ) -> File:
-        """
-        Uploads a file to a bucket.
-
-        ...
+        """Uploads a file to a bucket.
 
         Parameters
         -----------
-        content_bytes: bytes
+        content_bytes: :class:`bytes`
             The raw bytes of the file to be uploaded.
-        content_type: str
+        content_type: :class:`str`
             The content type of the content_bytes, e.g. video/mp4.
-        file_name: str
+        file_name: :class:`str`
             The name of the file.
-        bucket_id: str
+        bucket_id: :class:`str`
             The ID of the bucket to upload to.
+
         Returns
         ---------
-        File
-            A File object wrapping the data provided by Backblaze.
+        :class:`File`
+            The uploaded file.
         """
-        data = await self._http._upload_file(
+
+        data = await (await self._http.upload_file(
             content_bytes=content_bytes,
             content_type=content_type,
             file_name=file_name,
             bucket_id=bucket_id
-        )
-
-        return File.from_response(data)
+        ))
+        return File(data)
 
     async def delete_file(
             self,
             file_name: str,
             file_id: str
     ) -> DeletedFile:
-        """
-        Deletes a file from a bucket.
-
-        ...
+        """Deletes a file from a bucket.
 
         Parameters
         -----------
-        file_name: str
+        file_name: :class:`str`
             The name of the file to delete.
-        file_id: str
+        file_id: :class:`str`
             The id of the file to delete.
+
         Returns
         ---------
-        DeletedFile
-            Returns a DeletedFile object with the attributes `file_name` and `file_id`.
+        :class:`DeletedFile`
+            The deleted file.
         """
 
-        data = await self._http._delete_file(file_name=file_name, file_id=file_id)
-
-        return DeletedFile.from_response(data)
+        data = await self._http.delete_file(file_name=file_name, file_id=file_id)
+        return DeletedFile(data)
 
     async def download_file_by_id(
         self,
@@ -102,35 +103,34 @@ class Client:
         content_type: Optional[str] = None,
         server_side_encryption: Optional[str] = None
     ) -> DownloadedFile:
-        """
-        Downloads a file.
-
-        ...
+        """Downloads a file.
 
         Parameters
         -----------
-            file_id: str
-                The file id of the file to be downloaded.
-            content_disposition: Optional[str]
-                Overrides the current 'b2-content-disposition' specified when the file was uploaded.
-            content_language: Optional[str]
-                Overrides the current 'b2-content-language' specified when the file was uploaded.
-            expires: Optional[str]
-                Overrides the current 'b2-expires' specified when the file was uploaded.
-            cache_control: Optional[str]
-                Overrides the current 'b2-cache-control' specified when the file was uploaded.
-            content_encoding: Optional[str]
-                Overrides the current 'b2-content-encoding' specified when the file was uploaded.
-            content_type: Optional[str]
-                Overrides the current 'Content-Type' specified when the file was uploaded.
-            server_side_encryption: Optional[str]
-                This is requires if the file was uploaded and stored using Server-Side Encryption with
-                Customer-Managed Keys (SSE-C)
+        file_id: :class:`str`
+            The file id of the file to be downloaded.
+        content_disposition: Optional[:class:`str`]
+            Overrides the current 'b2-content-disposition' specified when the file was uploaded.
+        content_language: Optional[:class:`str`]
+            Overrides the current 'b2-content-language' specified when the file was uploaded.
+        expires: Optional[:class:`str`]
+            Overrides the current 'b2-expires' specified when the file was uploaded.
+        cache_control: Optional[:class:`str`]
+            Overrides the current 'b2-cache-control' specified when the file was uploaded.
+        content_encoding: Optional[:class:`str`]
+            Overrides the current 'b2-content-encoding' specified when the file was uploaded.
+        content_type: Optional[:class:`str`]
+            Overrides the current 'Content-Type' specified when the file was uploaded.
+        server_side_encryption: Optional[:class:`str`]
+            This is requires if the file was uploaded and stored using Server-Side Encryption with
+            Customer-Managed Keyts (SSE-C)
+
         Returns
         ---------
-        DownloadedFile
-            A DownloadedFile object containing the data Backblaze sent.
+        :class:`DownloadedFile`
+            The file requested.
         """
+
         data = await self._http.download_file_by_id(
             file_id=file_id,
             content_disposition=content_disposition,
@@ -141,8 +141,7 @@ class Client:
             content_type=content_type,
             server_side_encryption=server_side_encryption
         )
-
-        return DownloadedFile.from_response(data[1], data[0])
+        return DownloadedFile(data[0], data[1])  # type: ignore
 
     async def download_file_by_name(
         self,
@@ -157,38 +156,37 @@ class Client:
         content_type: Optional[str] = None,
         server_side_encryption: Optional[str] = None
     ) -> DownloadedFile:
-        """
-        Downloads a file.
-
-        ...
+        """Downloads a file.
 
         Parameters
         -----------
-            file_name: Optional[str]
-                The file name of the file to be downloaded.
-            bucket_name: Optional[str]
-                The bucket name of the file to be downloaded. This should only be specified if you have specified
-                file_name and not file_id.
-            content_disposition: Optional[str]
-                Overrides the current 'b2-content-disposition' specified when the file was uploaded.
-            content_language: Optional[str]
-                Overrides the current 'b2-content-language' specified when the file was uploaded.
-            expires: Optional[str]
-                Overrides the current 'b2-expires' specified when the file was uploaded.
-            cache_control: Optional[str]
-                Overrides the current 'b2-cache-control' specified when the file was uploaded.
-            content_encoding: Optional[str]
-                Overrides the current 'b2-content-encoding' specified when the file was uploaded.
-            content_type: Optional[str]
-                Overrides the current 'Content-Type' specified when the file was uploaded.
-            server_side_encryption: Optional[str]
-                This is requires if the file was uploaded and stored using Server-Side Encryption with
-                Customer-Managed Keyts (SSE-C)
+        file_name: :class:`str`
+            The file name of the file to be downloaded.
+        bucket_name: :class:`str`
+            The bucket name of the file to be downloaded. This should only be specified if you have specified
+            file_name and not file_id.
+        content_disposition: Optional[:class:`str`]
+            Overrides the current 'b2-content-disposition' specified when the file was uploaded.
+        content_language: Optional[:class:`str`]
+            Overrides the current 'b2-content-language' specified when the file was uploaded.
+        expires: Optional[:class:`str`]
+            Overrides the current 'b2-expires' specified when the file was uploaded.
+        cache_control: Optional[:class:`str`]
+            Overrides the current 'b2-cache-control' specified when the file was uploaded.
+        content_encoding: Optional[:class:`str`]
+            Overrides the current 'b2-content-encoding' specified when the file was uploaded.
+        content_type: Optional[:class:`str`]
+            Overrides the current 'Content-Type' specified when the file was uploaded.
+        server_side_encryption: Optional[:class:`str`]
+            This is requires if the file was uploaded and stored using Server-Side Encryption with
+            Customer-Managed Keyts (SSE-C)
+
         Returns
         ---------
-        DownloadedFile
-            A DownloadedFile object containing the data Backblaze sent.
+        :class:`DownloadedFile`
+            The file requested.
         """
+
         data = await self._http.download_file_by_name(
             file_name=file_name,
             bucket_name=bucket_name,
@@ -200,5 +198,4 @@ class Client:
             content_type=content_type,
             server_side_encryption=server_side_encryption
         )
-
-        return DownloadedFile.from_response(data[1], data[0])
+        return DownloadedFile(data[0], data[1])  # type: ignore
