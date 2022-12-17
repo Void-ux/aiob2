db9c404ffdbc15493baea3195fc94991fb6753cb
Void-ux <daniel.grant2017@gmail.com>
2022-12-17 23:37:01 +0800

    version bump, remove star imports

--- aiob2/__init__.py
+++ aiob2/__init__.py
@@ -1,8 +1,15 @@
-from .exceptions import *
-from .models import *
-from .bucket import Client  # type: ignore
+from .errors import (
+    RateLimited,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    BackblazeServerError,
+    HTTPException
+)
+from .models import File, DeletedFile, DownloadedFile
+from .bucket import Client
 
 __title__ = 'aiob2'
 __author__ = 'Dan'
 __license__ = 'MIT'
-__version__ = '0.4.4'
+__version__ = '0.5.0'
