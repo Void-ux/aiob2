f62e354c5b47bf6f3b5d1a2d2cccd5370ac2253a
Void-ux <daniel.grant2017@gmail.com>
2022-12-18 10:20:15 +0800

    feat: add docstrings refactor: uploaded_at -> created; date -> downloaded_at

--- aiob2/models/file.py
+++ aiob2/models/file.py
@@ -15,7 +15,7 @@ class PartialFilePayload(TypedDict):
 
 class UploadPayload(PartialFilePayload):
     accountId: str
-    action: Literal['start', 'upload', 'hide', 'folder']
+    action: Literal['upload']
     bucketId: str
     contentLength: Optional[int]
     contentSha1: Optional[str]
@@ -42,6 +42,15 @@ DownloadPayloadHeaders = TypedDict('DownloadPayloadHeaders', {
 
 
 class PartialFile(B2Object):
+    """Represents a "partial" file from Backblaze.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        The file's name.
+    id: :class:`str`
+        The file's ID.
+    """
     def __init__(
         self,
         data: PartialFilePayload
@@ -63,6 +72,41 @@ class PartialFile(B2Object):
 
 
 class File(PartialFile):
+    """Represents a file uploaded to Backblaze.
+
+    Attributes
+    ----------
+    account_id: :class:`str`
+        The account's ID that owns the file.
+    action: Literal[``'upload'``]
+        This will always be ``upload``.
+    bucket_id: :class:`str`
+        The bucket's ID that the file is in.
+    content_length: :class:`int`
+        The file's size represented in number of bytes.
+    content_sha1: :class:`str`
+        The file's SHA-1.
+    content_md5: Optional[:class:`str`]
+        The MD5 of the file's bytes as a 40-digit hex string.
+    content_type: :class:`str`
+        The file's content type, e.g. image/jpeg.
+    id: :class:`str`
+        The file's ID.
+    info: :class:`dict`
+        Any info regarding the file submitted at upload.
+    name: :class:`str`
+        The file's name.
+    retention: Optional[:class:`dict`]
+        The file's object lock retention settings.
+    legal_hold: Optional[:class:`dict`]
+        The file's object lock legal hold status.
+    replication_status: Optional[Literal[``'PENDING'``, ``'COMPLETED'``, ``'FAILED'``, ``'REPLICA'``]
+        The file's replication status.
+    server_side_encryption: Optional[:class:`dict`]
+        The file's encryption mode, and algorithm.
+    created: :class:`datetime.datetime`
+        The time at which the file was uploaded.
+    """
     def __init__(
         self,
         data: UploadPayload
@@ -75,7 +119,7 @@ class File(PartialFile):
         assert data['contentType'] is not None
 
         self.account_id: str = data['accountId']
-        self.action: str = data['action']
+        self.action: Literal['upload'] = data['action']
         self.bucket_id: str = data['bucketId']
         self.content_length: int = data['contentLength']
         self.content_sha1: str = data['contentSha1']
@@ -84,12 +128,21 @@ class File(PartialFile):
         self.info: dict = data['fileInfo']
         self.retention: Optional[dict] = data.get('fileRetention')
         self.legal_hold: Optional[dict] = data.get('legalHold')
-        self.replication_status: Optional[str] = data.get('replicationStatus')
+        self.replication_status: Optional[Literal['PENDING', 'COMPLETED', 'FAILED', 'REPLICA']] = data.get('replicationStatus')  # noqa
         self.server_side_encryption: Optional[dict] = data.get('serverSideEncryption')
-        self.upload_timestamp: datetime.datetime = format_timestamp(data['uploadTimestamp'])
+        self.created: datetime.datetime = format_timestamp(data['uploadTimestamp'])
 
 
 class DeletedFile(PartialFile):
+    """Represents a deleted file from Backblaze.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        The file's name.
+    id: :class:`str`
+        The file's ID.
+    """
     def __init__(
         self,
         data: PartialFilePayload
@@ -98,6 +151,27 @@ class DeletedFile(PartialFile):
 
 
 class DownloadedFile(B2Object):
+    """Represents a file downloaded from Backblaze.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        The file's name.
+    id: :class:`str`
+        The file's ID.
+    content_type: :class:`str`
+        The file's content type, e.g. image/jpeg.
+    content_length: :class:`int`
+        The file's size represented in number of bytes.
+    content_sha1: :class:`str`
+        The file's SHA-1.
+    created: :class:`datetime.datetime`
+        The time at which the file was uploaded.
+    downloaded_at: :class:`str`
+        The date at which the download was requested.
+    content: :class:`bytes`
+        The raw bytes of the downloaded file.
+    """
     def __init__(
         self,
         content: bytes,
@@ -108,8 +182,8 @@ class DownloadedFile(B2Object):
         self.id: str = headers['X-Bz-File-Id']
         self.name: str = headers['X-Bz-File-Name']
         self.content_sha1: str = headers['X-Bz-Content-Sha1']
-        self.upload_timestamp: datetime.datetime = format_timestamp(int(headers['X-Bz-Upload-Timestamp']))
-        self.date: datetime.datetime = datetime.datetime.strptime(
+        self.created: datetime.datetime = format_timestamp(int(headers['X-Bz-Upload-Timestamp']))
+        self.downloaded_at: datetime.datetime = datetime.datetime.strptime(
             headers['Date'],
             '%a, %d %b %Y %H:%M:%S %Z'
         )
